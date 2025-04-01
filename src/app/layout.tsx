4b5b976b8cd7b0d8151da34c8c0fd8bff6bb99a1
Youmna Ahmed <107641709+Youmnaelzany@users.noreply.github.com>
2025-04-01 16:58:14 +0200

    Add new assets, components, and configuration files; remove unused files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Kumbh_Sans } from "next/font/google";
+
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const kumbhSans = Kumbh_Sans({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
 export const metadata: Metadata = {
@@ -25,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${kumbhSans.className} mx-auto flex h-screen w-full max-w-7xl flex-col items-center justify-center bg-[#FAFAFA] antialiased`}
       >
         {children}
       </body>
